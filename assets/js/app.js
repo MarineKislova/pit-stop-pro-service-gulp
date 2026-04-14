769bddaebd566f27d96c6e60901c72733bcdbcb7
Marine <marinekislova@gmail.com>
2026-04-14 17:11:43 +0300

    2026/04/14

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-    //модальное окно для связи из header
+  //модальное окно для связи из header
   const openModalBtn = document.querySelectorAll(".open-modal");
   const modal = document.getElementById("contact-modal");
   const closeBtn = modal.querySelector(".modal__close");
@@ -45,5 +45,56 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.addEventListener("keydown", (e) => {
     if (e.key === "Escape") closeModal();
+  });
+
+  // аккордеон
+  function initAccordion() {
+    const items = document.querySelectorAll(".accordion__item");
+
+    items.forEach((item) => {
+      const header = item.querySelector(".accordion-toggle");
+      const body = item.querySelector(".accordion__item-body");
+      const icon = item.querySelector(".accordion-icon");
+
+      header.addEventListener("click", () => {
+        const isOpen = item.classList.contains("active");
+
+        // 1. Закрываем все открытые элементы
+        items.forEach((el) => {
+          el.classList.remove("active");
+          el.querySelector(".accordion__item-body").style.display = "none";
+          el.querySelector(".accordion-icon").textContent = "+";
+        });
+
+        // 2. Если текущий не был открыт — открываем его
+        if (!isOpen) {
+          item.classList.add("active");
+          body.style.display = "flex";
+          icon.textContent = "-";
+        }
+      });
+    });
+  }
+
+  // Запуск
+  initAccordion();
+
+
+    //скролл кнопка
+  const scrollTopBtn = document.getElementById("scrollTop");
+
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 500) {
+      scrollTopBtn.classList.add("_active");
+    } else {
+      scrollTopBtn.classList.remove("_active");
+    }
+  });
+
+  scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   });
 });
